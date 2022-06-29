dd7710044f74adf5d0330a90ff8ec5e8b7f480a0
Luiz Junior <luizjunior-cte@hotmail.com>
2022-06-29 00:32:26 -0300

    alterado espaço

--- src/main/java/com/AcademiaRestJPA/AcademiaRestJpaApplication.java
+++ src/main/java/com/AcademiaRestJPA/AcademiaRestJpaApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AcademiaRestJpaApplication {
+public class AcademiaRestJpaApplication{
 
 	public static void main(String[] args) {
 		SpringApplication.run(AcademiaRestJpaApplication.class, args);
